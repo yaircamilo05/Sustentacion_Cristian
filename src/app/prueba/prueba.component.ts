79c8e4674ba520e74e185162cda400fd46d1877b
Yair Sanchez <yair.sanchez21369@ucaldas.edu.co>
2023-09-18 08:52:33 -0500

    segundo commit

--- src/app/prueba/prueba.component.ts
+++ src/app/prueba/prueba.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
-
+import {Parser} from 'expr-eval';
 @Component({
   selector: 'app-prueba',
   templateUrl: './prueba.component.html',
   styleUrls: ['./prueba.component.css']
 })
 export class PruebaComponent {
-  
+
+  calcular: string="";
+  texto: string = ''; 
     constructor(
       private router: Router
     ) { }
@@ -19,4 +21,100 @@ export class PruebaComponent {
   
     ngOnInit(): void {
     }
+    numero1(){
+      this.calcular=this.calcular+"1";
+      this.texto=this.calcular;
+      
+    }
+    numero2(){
+      this.calcular=this.calcular+"2";
+      this.texto=this.calcular;
+      
+    }
+    numero3(){
+      this.calcular=this.calcular+"3";
+      this.texto=this.calcular;
+      
+    }
+    numero4(){
+      this.calcular=this.calcular+"4";
+      this.texto=this.calcular;
+      
+    }
+    numero5(){
+      this.calcular=this.calcular+"5";
+      this.texto=this.calcular;
+      
+    }
+    numero6(){
+      this.calcular=this.calcular+"6";
+      this.texto=this.calcular;
+      
+    }
+    numero7(){
+      this.calcular=this.calcular+"7";
+      this.texto=this.calcular;
+      
+    }
+    numero8(){
+      this.calcular=this.calcular+"8";
+      this.texto=this.calcular;
+      
+    }
+    numero9(){
+      this.calcular=this.calcular+"9";
+      this.texto=this.calcular;
+      
+    }
+    numero0(){
+      this.calcular=this.calcular+"0";
+      this.texto=this.calcular;
+      
+    }
+    Resta(){
+      this.calcular=this.calcular+"-";
+      this.texto=this.calcular;
+      
+    }
+    Multiplicacion(){
+      this.calcular=this.calcular+"*";
+      this.texto=this.calcular;
+      
+    }
+  
+    Division(){
+      this.calcular=this.calcular+"/";
+      this.texto=this.calcular;
+      
+    }
+  
+  
+    Suma(){
+      this.calcular=this.calcular+"+";
+      this.texto=this.calcular;
+      
+    }
+    Resultado(){
+      try {
+        var parser = new Parser();
+        var expr = parser.parse(this.calcular);
+        this.texto=expr.evaluate({});
+      
+        
+      } catch (error) {
+        alert("Error en la operacion");
+      }
+      
+      
+  
+    }
+    Limpiar(){
+      this.calcular="";
+      this.texto=this.calcular;
+    }
+    BorrarUNO(){
+      this.calcular=this.calcular.slice(0,-1);
+      this.texto=this.calcular;
+    }
+  
 }
